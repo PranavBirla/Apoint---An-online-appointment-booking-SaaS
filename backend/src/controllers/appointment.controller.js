2b22c6f4ea1b03a2d542382f77026b5099470a46
Pranav Birla <pranavbirla06@gmail.com>
2026-06-15 23:06:41 +0530

    Made update status APIs and filteration system. Also resolved some bugs

--- backend/src/controllers/appointment.controller.js
+++ backend/src/controllers/appointment.controller.js
@@ -103,6 +103,296 @@ async function createAppointment(req, res) {
 }
 
 
+async function getMyAppointments(req, res) {
+    try {
+        const { status } = req.query;
+
+        const validStatuses = [
+            "pending",
+            "confirmed",
+            "completed",
+            "cancelled"
+        ];
+        
+        if (
+            status &&
+            !validStatuses.includes(status)
+        ) {
+            return res.status(400).json({
+                message: "Invalid status"
+            });
+        }
+
+        const query = {
+            clientId: req.user._id
+        };
+
+        if (status) {
+            query.status = status;
+        }
+
+        const appointments = await appointmentModel.find(query)
+            .populate({
+                path: "professionalId",
+                populate: {
+                    path: "userId",
+                    select: "username email avatar"
+                }
+            });
+
+        const formattedAppointments = appointments.map(
+            appointment => {
+                return {
+                    appointmentId: appointment._id,
+
+                    professionalName:
+                        appointment.professionalId.userId.username,
+
+                    profession:
+                        appointment.professionalId.profession,
+
+                    appointmentDate:
+                        appointment.appointmentDate,
+
+                    startTime:
+                        appointment.startTime,
+
+                    endTime:
+                        appointment.endTime,
+
+                    status:
+                        appointment.status
+                };
+            }
+        )
+
+        res.status(200).json({
+            appointments: formattedAppointments
+        })
+    } catch (error) {
+        console.log(error);
+
+        res.status(500).json({
+            message: "Something went wrong"
+        });
+    }
+
+}
+
+
+async function getProfessionalAppointments(req, res) {
+    try {
+        const { status } = req.query;
+
+        const validStatuses = [
+            "pending",
+            "confirmed",
+            "completed",
+            "cancelled"
+        ];
+
+        if (
+            status &&
+            !validStatuses.includes(status)
+        ) {
+            return res.status(400).json({
+                message: "Invalid status"
+            });
+        }
+
+        const profile = await professionalProfileModel.findOne({ userId: req.user._id });
+
+        if (!profile) {
+            return res.status(404).json({
+                message: "Professional profile not found"
+            });
+        }
+
+        const query = {
+            professionalId: profile._id
+        };
+
+        if (status) {
+            query.status = status;
+        }
+
+        const appointments = await appointmentModel.find(query)
+            .populate("clientId", "username email avatar");
+
+
+
+        const formattedAppointments = appointments.map(
+            appointment => {
+                return {
+                    appointmentId: appointment._id,
+
+                    clientName:
+                        appointment.clientId.username,
+
+                    clientEmail:
+                        appointment.clientId.email,
+
+                    clientAvatar:
+                        appointment.clientId.avatar,
+
+                    appointmentDate:
+                        appointment.appointmentDate,
+
+                    startTime:
+                        appointment.startTime,
+
+                    endTime:
+                        appointment.endTime,
+
+                    status:
+                        appointment.status
+                };
+            }
+        )
+
+        res.status(200).json({
+            appointments: formattedAppointments
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            message: "Something went wrong!"
+        })
+    }
+}
+
+
+async function updateAppointmentStatus(req, res) {
+    try {
+
+        const { id } = req.params;
+        const { status } = req.body;
+
+        const validStatuses = [
+            "pending",
+            "confirmed",
+            "completed"
+        ];
+
+        if (!validStatuses.includes(status)) {
+            return res.status(400).json({
+                message: "Invalid status"
+            });
+        }
+
+        const profile =
+            await professionalProfileModel.findOne({
+                userId: req.user._id
+            });
+
+        const appointment =
+            await appointmentModel.findById(id);
+
+        if (!appointment) {
+            return res.status(404).json({
+                message: "Appointment not found"
+            });
+        }
+
+        if (
+            appointment.professionalId.toString()
+            !==
+            profile._id.toString()
+        ) {
+            return res.status(403).json({
+                message: "Unauthorized"
+            });
+        }
+
+        if (
+            appointment.status === "cancelled" ||
+            appointment.status === "completed"
+        ) {
+            return res.status(400).json({
+                message: "Appointment can no longer be modified"
+            });
+        }
+
+        const allowedTransitions = {
+            pending: ["confirmed", "cancelled"],
+            confirmed: ["completed", "cancelled"],
+            completed: [],
+            cancelled: []
+        };
+
+        if (
+            !allowedTransitions[
+                appointment.status
+            ].includes(status)
+        ) {
+            return res.status(400).json({
+                message:
+                `Cannot change status from ${appointment.status} to ${status}`
+            });
+        }
+
+        appointment.status = status;
+
+        await appointment.save();
+
+        res.status(200).json({
+            message: "Status updated successfully",
+            appointment
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            message: "Something went wrong!"
+        })
+    }
+}
+
+async function cancelAppointment(req, res) {
+    try {
+
+        const { id } = req.params;
+
+        const appointment =
+            await appointmentModel.findById(id);
+
+        if (!appointment) {
+            return res.status(404).json({
+                message: "Appointment not found"
+            });
+        }
+
+        if (
+            appointment.clientId.toString()
+            !==
+            req.user._id.toString()
+        ) {
+            return res.status(403).json({
+                message: "Unauthorized"
+            });
+        }
+
+        appointment.status = "cancelled";
+
+        await appointment.save();
+
+        res.status(200).json({
+            message:
+                "Appointment cancelled successfully",
+            appointment
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            message: "Something went wrong!"
+        })
+    }
+}
+
+
 module.exports = {
-    createAppointment
+    createAppointment,
+    getMyAppointments,
+    getProfessionalAppointments,
+    updateAppointmentStatus,
+    cancelAppointment
 }
--- backend/src/routes/appointment.routes.js
+++ backend/src/routes/appointment.routes.js
@@ -2,8 +2,14 @@ const express = require("express");
 const availabilityController = require("../controllers/availability.controller");
 const appointmentController = require("../controllers/appointment.controller");
 const authMiddleware = require("../middlewares/auth.middleware");
+const roleMiddleware = require("../middlewares/role.middleware");
 const router = express.Router();
 
 router.post("/appointment", authMiddleware, appointmentController.createAppointment);
+router.get("/appointments/my", authMiddleware, appointmentController.getMyAppointments);
+router.get("/appointments/professional", authMiddleware, appointmentController.getProfessionalAppointments);
+
+router.patch("/appointments/:id/status", authMiddleware, roleMiddleware("professional"), appointmentController.updateAppointmentStatus);
+router.patch("/appointments/:id/cancel", authMiddleware, appointmentController.cancelAppointment);
 
 module.exports = router;
