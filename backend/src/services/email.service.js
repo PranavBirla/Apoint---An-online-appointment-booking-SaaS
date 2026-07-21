3eb005f20446920143d8f55299ab4a6aef562044
Pranav Birla <pranavbirla06@gmail.com>
2026-07-21 23:40:22 +0530

    Implemented IPv4 gmail smpt instead of IPv6

--- backend/src/services/email.service.js
+++ backend/src/services/email.service.js
@@ -1,13 +1,15 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-
-    service: "gmail",
-
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    requireTLS: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-    }
+    },
+    family: 4
 });
 
 transporter.verify((error, success) => {
